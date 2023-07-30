66ee884613ef38ed4dfd94f567c4b60a5e079f13
sirthebenjamin <126521851+sirthebenjamin@users.noreply.github.com>
2023-07-30 12:44:39 -0400

    Adds myfile.md

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        Add any content of your choosing here. For example, you can write about
+        your skills, experience, or interests.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
